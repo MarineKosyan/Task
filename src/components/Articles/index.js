7250a825b029af2d4db2bbde2054e200ee3b846f
MarineKosyan <47843582+MarineKosyan@users.noreply.github.com>
2019-07-01 07:56:05 +0400

    Update index.js

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -2,7 +2,6 @@ import React, { Fragment, PureComponent } from 'react';
 import axios from 'axios';
 
 import Article from './Article';
-import { PAGE_SIZE, MILLISECONDS } from "../../constants/defaults";
 import { getItemFromStorage } from "../../utils/localStorageHelpers";
 
 class Articles extends PureComponent {
@@ -11,19 +10,20 @@ class Articles extends PureComponent {
     this.state = {
       intervalId: 0,
       articles: [],
-      total: 0,
-      pageSize: PAGE_SIZE,
+      currentPage: 1,
+      pages: 0,
     };
   }
 
   componentDidMount() {
     this.request();
-    this.setState({ intervalId: setInterval(this.request, MILLISECONDS)});
-    window.addEventListener('scroll', this.handleOnScroll)
+    this.setState({ intervalId: setInterval(this.request, 30000)});
+    window.addEventListener('scroll', this.handleOnScroll);
+
   }
 
   componentWillUnmount() {
-    clearInterval(this.state.intervalId);
+    // clearInterval(this.state.intervalId);
     window.removeEventListener('scroll', this.handleOnScroll)
   }
 
@@ -40,22 +40,34 @@ class Articles extends PureComponent {
     </Fragment>;
   }
 
-  handleOnScroll = (e,c,f) => {
+  handleOnScroll = () => {
     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    console.log(height, window.scrollY);
     if (height === window.scrollY) {
-      const { total, pageSize } = this.state;
-      if(pageSize * PAGE_SIZE < total) {
-        this.request(pageSize + PAGE_SIZE);
+      const { currentPage, pages } = this.state;
+      if(currentPage < pages) {
+        this.request(currentPage + 1);
       }
     }
   };
 
-  request = (pageSize) => {
+  request = (page) => {
     const self = this;
-    axios.get(`https://content.guardianapis.com/search?api-key=test&format=json&show-fields=thumbnail&page-size=${(pageSize || this.state.pageSize)}`)
+    axios.get(`https://content.guardianapis.com/search?api-key=test&format=json&show-fields=thumbnail&page=${page || this.state.currentPage}`)
       .then(function (res) {
-        const {data: { response: { results, total, pageSize} }} = res;
-        self.setState({articles: results, total, pageSize });
+        const {data: { response: { results, currentPage, pages} }} = res;
+        if (page) {
+          self.setState({ articles: [...self.state.articles, ...results], currentPage, pages });
+        } else {
+          if (self.state.articles.length) {
+            const index = results.findIndex(item => self.state.articles[0].id === item.id);
+            if (index > 0) {
+              self.setState({ articles: [...results.slice(0, index), ...self.state.articles], currentPage, pages });
+            }
+          } else {
+            self.setState({ articles: results, currentPage, pages });
+          }
+        }
       })
       .catch(function (error) {
         console.log(error);
